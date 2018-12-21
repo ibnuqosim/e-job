fe1c89b8184cc7d3c6afbc964fc7570f80eafabc
ibnuqosim <ibnuqosim022@gmail.com>
2018-12-21 14:30:37 +0700

    master data

--- resources/views/menu/AdminAnalystOD/masterdata.blade.php
+++ resources/views/menu/AdminAnalystOD/masterdata.blade.php
@@ -1,6 +1,6 @@
 <li><a href="{{ url('AdminAnalystOD/list_matrikindikator') }}"><i class="fa fa-circle-o"></i>Matrikindikator</a></li>
 <li><a href="{{ url('AdminAnalystOD/list_bahankerja') }}"><i class="fa fa-circle-o"></i>Bahan Kerja</a></li>
-<li><a href="{{ url('AdminAnalystOD/list_katakerja') }}"><i class="fa fa-circle-o"></i>Kata Kerja</a></li>
+<li><a href="{{ url('AdminAnalystOD/list_katakerja') }}"><i class="fa fa-circle-o"></i>Kata Kerja (Main Responsibility)</a></li>
 <li><a href="{{ url('AdminAnalystOD/list_pengalamankerja') }}"><i class="fa fa-circle-o"></i>pengalaman kerja</a></li>
 <li><a href="{{ url('AdminAnalystOD/list_pendidikan') }}"><i class="fa fa-circle-o"></i>Pendidikan</a></li>
 <li><a href="{{ url('AdminAnalystOD/list_alatkerja') }}"><i class="fa fa-circle-o"></i>Alat Kerja</a></li>
