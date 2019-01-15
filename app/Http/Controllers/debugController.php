f6d2c741a175a3b03b30e34d5f682c40fa377f82
gumilargugum15 <gumilargugum15@gmail.com>
2019-01-15 10:36:59 +0700

    managercontroller,debug

--- app/Http/Controllers/debugController.php
+++ app/Http/Controllers/debugController.php
@@ -11,12 +11,12 @@ class debugController extends Controller
 {
    public function index(Request $request)
    {
-        $userid = Auth::user()->userid;
-        $data = User::where('userid',$userid)->first();
-        $ret = $data->structdisp->where('no','1');
+        $userid = Auth::user()->roles;
+      //   $data = User::where('userid',$userid)->first();
+      //   $ret = $data->structdisp->where('no','1');
         // foreach ($data->structdisp as $key => $value) {
         //    var_dump($value->empname);
         // }
-        return dd($ret);
+        return dd($userid);
    }
 }
