17194a94a8efd2b329f2d33ad7e83a39f013369a
gumilargugum15 <gumilargugum15@gmail.com>
2019-04-22 09:08:05 +0700

    avatar,adminlteblade

--- resources/views/layouts/adminLTE.blade.php
+++ resources/views/layouts/adminLTE.blade.php
@@ -161,7 +161,7 @@
         <!-- Sidebar user panel -->
         <div class="user-panel">
           <div class="pull-left image">
-            <img src="{{ url('adminlte/dist/img/user2-160x160.jpg') }}" class="img-circle" alt="User Image">
+            <img src="{{ url('adminlte/dist/img/user2-160x160.png') }}" class="img-circle" alt="User Image">
           </div>
           <div class="pull-left info">
             <p>{{ Auth::user()->username }}</p>
